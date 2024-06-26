f66fa94f882a72ec9f651d3d33e366ed62f6e14d
Ivica Baricevic <baricevic@gmail.com>
2024-06-25 21:56:18 -0400

    Add routing and 404 handling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AlgebraicStructuresApp from './AlgebraicStructuresApp';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <AlgebraicStructuresApp />
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<AlgebraicStructuresApp />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
